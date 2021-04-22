fb550e0c0eb93e0a5efcbca8bc34933c388fdbe4
Rendy Setiadi <rendysetiady8@gmail.com>
2021-04-22 08:55:33 +0700

    Feat: Animated Parallel, Squence, Stagger, Delay

--- App.js
+++ App.js
@@ -1,79 +1,103 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Animated, PanResponder} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animation: new Animated.ValueXY(0),
+      colorAnimation: new Animated.Value(0),
+      scaleAnimation: new Animated.Value(1),
     };
-    // this.x = 0;
-    // this.y = 0;
-
-    // this.state.animation.addListener(value => {
-    //   this.x = value.x,
-    //   this.y = value.y
-    // });
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        this.state.animation.extractOffset();
-        // this.state.animation.setOffset({
-        //   x: this.x,
-        //   y: this.y,
-        // });
-        // this.state.animation.setValue({
-        //   x: 0,
-        //   y: 0,
-        // });
-      },
-      onPanResponderMove: Animated.event([
-        null,
-        {
-          dx: this.state.animation.x,
-          dy: this.state.animation.y,
-        },
-      ]),
-      onPanResponderRelease: (e, {vx, vy}) => {
-        Animated.decay(this.state.animation, {
-          velocity: {x: vx, y: vy},
-          deceleration: 0.997,
-        }).start();
-      },
-    });
   }
 
-  componentDidMount() {
-    // this._panResponder = PanResponder.create({
-    //   onStartShouldSetPanResponder: () => true,
-    //   onMoveShouldSetPanResponder: () => true,
-    //   onPanResponderMove: Animated.event([
-    //     null,
-    //     {
-    //       dx: this.state.animation.x,
-    //       dy: this.state.animation.y,
-    //     },
-    //   ]),
-    //   onPanResponderRelease: (e, {vx, vy}) => {
-    //     Animated.decay(this.state.animation, {
-    //       velocity: {x: vx, y: vy},
-    //       deceleration: 0.997,
-    //     }).start();
-    //   },
-    // });
-  }
+  handlePress = () => {
+    // PARALLEL
+    // Animated.parallel([
+    //   Animated.timing(this.state.colorAnimation, {
+    //     toValue: 1,
+    //     duration: 500,
+    //   }),
+    //   Animated.timing(this.state.scaleAnimation, {
+    //     toValue: 2,
+    //     duration: 300,
+    //     // useNativeDriver: true,
+    //   }),
+    // ]).start();
+
+    // SQUENCE
+    // Animated.sequence([
+    //   Animated.timing(this.state.colorAnimation, {
+    //     toValue: 1,
+    //     duration: 500,
+    //   }),
+    //   Animated.timing(this.state.scaleAnimation, {
+    //     toValue: 2,
+    //     duration: 300,
+    //     // useNativeDriver: true,
+    //   }),
+    // ]).start();
+
+    // //STAGGER
+    // Animated.stagger(4000, [
+    //   Animated.timing(this.state.colorAnimation, {
+    //     toValue: 1,
+    //     duration: 500,
+    //   }),
+    //   Animated.timing(this.state.scaleAnimation, {
+    //     toValue: 2,
+    //     duration: 300,
+    //     // useNativeDriver: true,
+    //   }),
+    // ]).start();
+    
+    // DELAY
+    Animated.sequence([
+      Animated.timing(this.state.colorAnimation, {
+        toValue: 1,
+        duration: 500,
+      }),
+      Animated.timing(this.state.scaleAnimation, {
+        toValue: 2,
+        duration: 300,
+        // useNativeDriver: true,
+      }),
+      Animated.delay(1500),
+      Animated.parallel([
+        Animated.timing(this.state.colorAnimation, {
+          toValue: 0,
+          duration: 500,
+        }),
+        Animated.timing(this.state.scaleAnimation, {
+          toValue: 1,
+          duration: 300,
+          // useNativeDriver: true,
+        }),
+      ])
+    ]).start();
+  };
 
   render() {
-    const animatedStyle = {
-      transform: this.state.animation.getTranslateTransform(),
+    const backgroundColorIntepolated = this.state.colorAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['rgb(255,99,71)', 'rgb(99,71,255)'],
+    });
+    const boxStyle = {
+      backgroundColor: backgroundColorIntepolated,
+      transform: [{scale: this.state.scaleAnimation}],
     };
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={[styles.box, animatedStyle]}
-          {...this._panResponder.panHandlers}
-        />
+        <TouchableWithoutFeedback onPress={this.handlePress}>
+          <Animated.View style={[styles.box, boxStyle]}>
+            <Text style={styles.text}>Hello Parallel</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -82,9 +106,14 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
   box: {
-    width: 50,
-    height: 50,
+    width: 150,
+    height: 150,
     backgroundColor: 'tomato',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
   },
 });
 
