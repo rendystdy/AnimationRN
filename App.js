b9680b86f49551811fc86af207b9861d84418f9c
Rendy Setiadi <rendysetiady8@gmail.com>
2021-04-24 23:48:40 +0700

    Feat: add example setNativeProps

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Animated, Button} from 'react-native';
-
-const AnimatedButton = Animated.createAnimatedComponent(Button);
+import {
+  Text,
+  View,
+  StyleSheet,
+  Animated,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       animation: new Animated.Value(0),
     };
+    this._enabled = true;
   }
+
+  handleToggle = () => {
+    this._enabled = !this._enabled;
+    let style = [styles.scroll];
+
+    if (!this._enabled) {
+      style.push(styles.hide);
+    } else {
+      // style.push(styles.show)
+    }
+
+    this._scroll.setNativeProps({
+      scrollEnabled: this._enabled,
+      style,
+    });
+  };
 
   startAnimation = () => {
     Animated.timing(this.state.animation, {
@@ -23,18 +45,35 @@ class App extends Component {
   };
 
   render() {
-    const animatedColor = this.state.animation.interpolate({
-      inputRange: [0, 1],
+    const bgInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 3000],
       outputRange: ['rgb(255,99,71)', 'rgb(99,71,255)'],
     });
+    const scrollStyle = {
+      backgroundColor: bgInterpolate,
+    };
 
     return (
       <View style={styles.container}>
-        <AnimatedButton
-          title="Press Me"
-          onPress={this.startAnimation}
-          color={animatedColor}
-        />
+        <TouchableOpacity onPress={this.handleToggle}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+
+        <ScrollView
+          style={styles.scroll}
+          ref={scroll => (this._scroll = scroll)}
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  y: this.state.animation,
+                },
+              },
+            },
+          ])}>
+          <Animated.View style={[styles.fakeContent, scrollStyle]} />
+        </ScrollView>
       </View>
     );
   }
@@ -43,8 +82,21 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  scroll: {
+    flex: 1,
+    opacity: 1,
+  },
+  hide: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+  },
+  fakeContent: {
+    height: 3000,
+    backgroundColor: 'tomato',
   },
 });
 
