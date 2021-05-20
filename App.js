ca042f19043633737418ec649f75ae0098168cbb
Rendy Setiadi <rendysetiady8@gmail.com>
2021-05-20 14:34:59 +0700

    Feat: add example staggerHeads

--- App.js
+++ App.js
@@ -5,66 +5,91 @@ import {
   Animated,
   TouchableWithoutFeedback,
   Dimensions,
+  PanResponder,
+  Image,
+  Text
 } from 'react-native';
+
+import Vjeux from './vjeux.jpg';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animation: new Animated.ValueXY(),
+      heads: [
+        {
+          image: Vjeux,
+          animation: new Animated.ValueXY(),
+          text: 'Drag Me',
+        },
+        {
+          image: Vjeux,
+          animation: new Animated.ValueXY(),
+        },
+        {
+          image: Vjeux,
+          animation: new Animated.ValueXY(),
+        },
+        {
+          image: Vjeux,
+          animation: new Animated.ValueXY(),
+        },
+      ],
     };
+    
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        this.state.heads.map(({animation}) => {
+          animation.extractOffset();
+          // setValue Animated bug fix
+          animation.setValue({x: 0, y: 0});
+        });
+      },
+      onPanResponderMove: (e, {dx, dy}) => {
+        this.state.heads[0].animation.setValue({
+          x: dx,
+          y: dy,
+        });
+
+        const animations = this.state.heads
+          .slice(1)
+          .map(({animation}, index) => {
+            return Animated.sequence([
+              Animated.delay(index * 20),
+              Animated.spring(animation, {
+                toValue: {x: dx, y: dy},
+              }),
+            ]).start();
+          });
+      },
+    });
   }
 
-  startAnimation = () => {
-    const {width, height} = Dimensions.get('window');
-
-    console.log({
-      height,
-      heightLayout: height - this._height,
-      width,
-      widthLayout: width - this._width,
-    });
-
-    Animated.sequence([
-      Animated.spring(this.state.animation.y, {
-        toValue: height - this._height,
-      }),
-      Animated.spring(this.state.animation.x, {
-        toValue: width - this._width,
-      }),
-      Animated.spring(this.state.animation.y, {
-        toValue: 0,
-      }),
-      Animated.spring(this.state.animation.x, {
-        toValue: 0,
-      }),
-    ]).start();
-  };
-
-  saveDimensions = e => {
-    console.log('saveDimensions()');
-    console.log({
-      heightLayout2: e.nativeEvent.layout.height,
-      widthLayout2: e.nativeEvent.layout.width,
-    });
-    this._width = e.nativeEvent.layout.width;
-    this._height = e.nativeEvent.layout.height;
-  };
-
-  componentDidMount() {}
-
   render() {
-    const animatedStyles = {
-      transform: this.state.animation.getTranslateTransform(),
-    };
-
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback
-          onPress={this.startAnimation}
-          onLayout={this.saveDimensions}>
-          <Animated.View style={[styles.box, animatedStyles]} />
-        </TouchableWithoutFeedback>
+        {this.state.heads
+          .slice(0)
+          .reverse()
+          .map((item, index, items) => {
+            const pan =
+              index === items.length - 1 ? this._panResponder.panHandlers : {};
+
+            return (
+              <Animated.View
+                {...pan}
+                key={index}
+                style={[
+                  styles.wrap,
+                  {transform: item.animation.getTranslateTransform()},
+                ]}>
+                <Image source={item.image} style={styles.head} resizeMode="cover" />
+                <Text>{item.text}</Text>
+              </Animated.View>
+            );
+          })}
       </View>
     );
   }
@@ -73,14 +98,25 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  box: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'tomato',
+  wrap: {
     position: 'absolute',
-    top: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    height: 80,
+  },
+  head: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    position: 'absolute',
     left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
