43d1355c54971ee3452571decc05b0e69588852a
Rendy Setiadi <rendysetiady8@gmail.com>
2021-06-09 14:32:30 +0700

    Feat: add example AppIntro animation

--- App.js
+++ App.js
@@ -1,17 +1,77 @@
 import React, {Component, useRef} from 'react';
 import {
   StyleSheet,
+  Text,
   View,
   Animated,
-  Text,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
+  ScrollView,
+  Dimensions,
+  PixelRatio,
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as Images from './images';
 
-Icon.loadFont();
+const getScreen1Styles = (animation, width) => {
+  const image2TranslateX = animation.interpolate({
+    inputRange: [0, width],
+    outputRange: [0, -160],
+    extrapolate: 'clamp',
+  });
 
+  return {
+    image2: {
+      transform: [{translateX: image2TranslateX}],
+    },
+  };
+};
+
+const getScreen2Styles = (animation, width) => {
+  const inputRange = [0, width, width * 2];
+
+  const image2TranslateY = animation.interpolate({
+    inputRange,
+    outputRange: [100, 0, -100],
+    extrapolate: 'clamp',
+  });
+
+  const image2Opacity = animation.interpolate({
+    inputRange,
+    outputRange: [0, 1, 0],
+    extrapolate: 'clamp',
+  });
+
+  return {
+    image2: {
+      opacity: image2Opacity,
+      transform: [{translateY: image2TranslateY}],
+    },
+  };
+};
+
+const getScreen3Styles = (animation, width) => {
+  const inputRange = [width, width * 2, width * 3];
+
+  const imageScale = animation.interpolate({
+    inputRange,
+    outputRange: [0, 1, 0],
+    extrapolate: 'clamp',
+  });
+
+  const image2Rotate = animation.interpolate({
+    inputRange,
+    outputRange: ['-180deg', '0deg', '180deg'],
+    extrapolate: 'clamp',
+  });
+
+  return {
+    image1: {
+      transform: [{scale: imageScale}],
+    },
+    image2: {
+      transform: [{scale: imageScale}, {rotate: image2Rotate}],
+    },
+  };
+};
 class App extends Component {
   constructor(props) {
     super(props);
@@ -20,93 +80,157 @@ class App extends Component {
     };
   }
 
-  toggleOpen = () => {
-    const toValue = this._open ? 0 : 1;
-
-    Animated.timing(this.state.animation, {
-      toValue,
-      duration: 200,
-    }).start();
-
-    this._open = !this._open;
-  };
-
   render() {
-    const bgStyle = {
-      transform: [
-        {
-          scale: this.state.animation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [0, 40],
-          }),
-        },
-      ],
-    };
-    const reloadStyle = {
-      transform: [
-        {scale: this.state.animation},
-        {
-          translateY: this.state.animation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [0, -70],
-          }),
-        },
-      ],
-    };
+    const {width, height} = Dimensions.get('window');
 
-    const orderStyle = {
-      transform: [
-        {scale: this.state.animation},
-        {
-          translateY: this.state.animation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [0, -140],
-          }),
-        },
-      ],
-    };
+    const screen1Styles = getScreen1Styles(this.state.animation, width);
 
-    const labelPositionInterpolate = this.state.animation.interpolate({
-      inputRange: [0, 1],
-      outputRange: [-30, -90],
-    });
+    const screen2Styles = getScreen2Styles(this.state.animation, width);
 
-    const opacityInterpolate = this.state.animation.interpolate({
-      inputRange: [0, 0.8, 1],
-      outputRange: [0, 0, 1],
-    });
+    const screen3Styles = getScreen3Styles(this.state.animation, width);
 
-    const labelStyle = {
-      opacity: opacityInterpolate,
-      transform: [{translateX: labelPositionInterpolate}],
-    };
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.background, bgStyle]} />
-        <TouchableWithoutFeedback>
-          <Animated.View style={[styles.button, styles.other, orderStyle]}>
-            <Animated.Text style={[styles.label, labelStyle]}>
-              Order
-            </Animated.Text>
-            <Icon name="food-fork-drink" size={20} color="#555" />
-          </Animated.View>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback>
-          <Animated.View style={[styles.button, styles.other, reloadStyle]}>
-            <Animated.Text style={[styles.label, labelStyle]}>
-              Reload
-            </Animated.Text>
-            <Icon name="reload" size={20} color="#555" />
-          </Animated.View>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback onPress={this.toggleOpen}>
-          <View style={[styles.button, styles.pay]}>
-            <Animated.Text style={[styles.label, labelStyle]}>
-              Pay
-            </Animated.Text>
-            <Text style={styles.payText}>$5.00</Text>
+        <ScrollView
+          style={styles.container}
+          pagingEnabled
+          horizontal
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: this.state.animation,
+                },
+              },
+            },
+          ])}>
+          <View style={{width, height, backgroundColor: '#F89E20'}}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Image1}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(75),
+                  height: PixelRatio.getPixelSizeForLayoutSize(63),
+                }}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: 'absolute',
+                    top: 220,
+                    left: 110,
+                  },
+                  screen1Styles.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: 'absolute',
+                  top: 185,
+                  left: 105,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 1</Text>
+            </View>
           </View>
-        </TouchableWithoutFeedback>
+
+          <View style={{width, height, backgroundColor: '#F89E20'}}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Image1}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(75),
+                  height: PixelRatio.getPixelSizeForLayoutSize(63),
+                }}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: 'absolute',
+                    top: 220,
+                    left: 110,
+                  },
+                  screen2Styles.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: 'absolute',
+                  top: 185,
+                  left: 105,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 2</Text>
+            </View>
+          </View>
+
+          <View style={{width, height, backgroundColor: '#F89E20'}}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Image1}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(75),
+                    height: PixelRatio.getPixelSizeForLayoutSize(63),
+                  },
+                  screen3Styles.image1,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: 'absolute',
+                    top: 220,
+                    left: 110,
+                  },
+                  screen3Styles.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: 'absolute',
+                  top: 185,
+                  left: 105,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 3</Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -116,43 +240,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  background: {
-    backgroundColor: 'rgba(0,0,0,.2)',
-    position: 'absolute',
-    width: 60,
-    height: 60,
-    bottom: 20,
-    right: 20,
-    borderRadius: 30,
-  },
-  button: {
-    width: 60,
-    height: 60,
+  screenHeader: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    shadowColor: '#333',
-    shadowOpacity: 0.1,
-    shadowOffset: {x: 2, y: 0},
-    shadowRadius: 2,
-    borderRadius: 30,
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
   },
-  other: {
-    backgroundColor: '#FFF',
-  },
-  payText: {
-    color: '#FFF',
-  },
-  pay: {
-    backgroundColor: '#00B15E',
-  },
-  label: {
-    color: '#fff',
-    position: 'absolute',
-    fontSize: 18,
-    backgroundColor: 'transparent',
+  screenText: {
+    flex: 1,
   },
 });
 
